8d7e1fd59950927a7f20eac57113b29be2d13e93
Sunat.P <31818562+SunatP@users.noreply.github.com>
2020-02-24 08:25:14 +0700

    update

---
+++ repacked.py
@@ -0,0 +1,10 @@
+import re
+import pprint
+# linear algebra
+import numpy as np 
+
+# data processing
+import pandas as pd 
+
+df = pd.read_csv('titanic.csv')
+pprint(df.head())
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,9 @@
+import re
+# linear algebra
+import numpy as np 
+
+# data processing
+import pandas as pd 
+
+df = pandas.read_csv("titanic.csv")
+df.head()
